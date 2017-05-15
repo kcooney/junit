ef1fde96adba60b8b4d18c8f66a9820811c07852
Kevin Cooney <kcooney@google.com>
2017-05-14 21:39:08 -0700

    Example implementation of Sorter.shuffledBy()

--- src/main/java/org/junit/runner/manipulation/Sorter.java
+++ src/main/java/org/junit/runner/manipulation/Sorter.java
@@ -1,6 +1,11 @@
 package org.junit.runner.manipulation;
 
 import java.util.Comparator;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.junit.runner.Description;
 
@@ -44,5 +49,83 @@ public class Sorter implements Comparator<Description> {
 
     public int compare(Description o1, Description o2) {
         return comparator.compare(o1, o2);
+    }
+
+    /**
+     * Creates an {@link Sorter} that shuffles the items using the given
+     * {@link Random} instance.
+     */
+    public static Sorter shuffledBy(Random random) {
+        return new Sorter(new ShufflingComparator(random));
+    }
+
+    private static class ShufflingComparator implements Comparator<Description> {
+        private final Random random;
+        private final ConcurrentMap<Description, RandomValueHolder> map
+                = new ConcurrentHashMap<Description, RandomValueHolder>();
+
+        private static class RandomValueHolder implements Comparable<RandomValueHolder> {
+            private static final Lock lock = new ReentrantLock();
+            private final Random random;
+            private volatile long[] randomValues;
+
+            RandomValueHolder(Random random) {
+                this.random = random;
+            }
+
+            long[] getRandomValues() {
+                long[] value = randomValues; // volatile read
+                if (value == null) {
+                    lock.lock();
+                    try {
+                        value = randomValues;
+                        if (value == null) {
+                            value = new long[4];
+                            for (int i = 0; i < value.length; i++) {
+                                value[i] = random.nextLong();
+                            }
+                            randomValues = value;
+                        }
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+                return value;
+            }
+
+            public int compareTo(RandomValueHolder other) {
+                long[] thisRandomValues = getRandomValues();
+                long[] otherRandomValues = other.getRandomValues();
+                for (int i = 0; i < thisRandomValues.length; i++) {
+                    int result = compare(thisRandomValues[i], otherRandomValues[i]);
+                    if (result != 0) {
+                        return result;
+                    }
+                }
+                return 0;
+            }
+
+            private static int compare(long one, long two) {
+                return (one < two) ? -1 : ((one == two) ? 0 : 1);
+            }
+        }
+
+        ShufflingComparator(Random random) {
+            this.random = random;
+        }
+
+        public int compare(Description o1, Description o2) {
+            RandomValueHolder holder = new RandomValueHolder(random);
+            RandomValueHolder h1 = map.putIfAbsent(o1, holder);
+            if (h1 == null) {
+                h1 = holder;
+                holder = new RandomValueHolder(random);
+            }
+            RandomValueHolder h2 = map.putIfAbsent(o2, holder);
+            if (h2 == null) {
+                h2 = holder;
+            }
+            return h1.compareTo(h2);
+        }
     }
 }
